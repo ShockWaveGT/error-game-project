a09d9c63e71b46fd9ac83b931bae694f49fb0b33
ShockWaveGT <nicooavenged@gmail.com>
2025-09-12 19:12:42 -0300

    Init commit
    Project Creation

---
+++ Source/ErrorGameProject/ErrorGameProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ErrorGameProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AErrorGameProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AErrorGameProjectGameMode();
+};
+
+
+
